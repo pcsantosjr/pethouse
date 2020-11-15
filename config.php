581015322b267a97ac48f3eade4cc9753e35e22c
Gregori Gomes Fernandes Vieira <gregori@datamotion.com.br>
2020-11-15 14:24:16 -0300

    Ajuste nome do banco de dados

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'db_pethouse');
+define('DB_NAME', 'pethouse');
  
 // Tentar conectar na DB
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
